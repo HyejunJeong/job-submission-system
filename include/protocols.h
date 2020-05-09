3cfbed1f37febce4ffddda9cece3fad60dde46a4
xiangshuai Gao <gaoxiangshuai@gmail.com>
2020-05-09 18:14:12 -0400

    added protocol.h

---
+++ include/protocols.h
@@ -0,0 +1,29 @@
+//
+// Created by xgao on 5/9/20.
+//
+
+#ifndef HW4_PROTOCOLS_H
+#define HW4_PROTOCOLS_H
+
+#include "constants.h"
+
+// you will need to malloc this struct to size of struct + envp size + the argv size to accommodate this
+typedef struct Job{
+    int maxMemory;
+    int maxTime;
+    int priority;
+    int envpSize; // use to extract envp
+    int argcSize; // use to extract argv
+    int argc; // argc of the job
+    byte envp[0]; // envp of the job
+    byte argv[0]; // argv of the job
+} Job;
+
+// like job will need to allocate size of the Packet + size of the Job
+typedef struct Packet{
+    byte commandType;
+    int msgSize; // This is the job struct size
+    Job job;
+} Packet;
+
+#endif //HW4_PROTOCOLS_H
