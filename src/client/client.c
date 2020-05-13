c6ebf13794376bd634ff270833292e0f6481edba
Hyejun <hye-jun.jeong@stonybrook.edu>
2020-05-13 13:35:02 +0900

    [client] fixed, need to be checked and clean up

--- src/client/client.c
+++ src/client/client.c
@@ -2,28 +2,184 @@
 // Created by xgao on 5/4/20.
 //
 #include <stdio.h>
-#include <sys/socket.h>
 #include <string.h>
-#include <sys/un.h>
 #include <unistd.h>
 #include <pwd.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 #include "../../include/constants.h"
 
-static void create_sock();
+#define DELIM   " \t\r\n\v\f"
+#define MAXLINE 32
+#define RECV_BUFF_SIZE 20480
+#define MYDBG       fprintf(stderr, "MYDBG:%s:%s:%d:\n",__FILE__,__func__,__LINE__)
+
 static int sock_fd;
 struct sockaddr_un server_addr;
-void onExitCallBack (int status, void* arg);
 
-int main() {
+static void create_sock();
+void print_buf(unsigned char *buf, int len);
+void print_usage();
+int submit(unsigned char buffer[BUFFER_SIZE], char **envp);
+int get_cmd_type(char *cmd, unsigned char packet[BUFFER_SIZE], char **envp);
 
-    // set up the socket
-    create_sock();
-    char buffer[BUFFER_SIZE];
-    strcpy(buffer, "testing");
-    int sent_bytes = send(sock_fd, buffer, BUFFER_SIZE, 0);
-    return 0;
+void print_usage(){
+    printf("Usage: submit, list, kill\n");
+    exit(0);
+}
+
+
+// prepare the job buffer upon submit cmd entered, returns joblen
+int submit(unsigned char buffer[BUFFER_SIZE], char **envp) {
+    int joblen = 0, argvlen = 0, envplen = strlen(*envp) + 1;
+    char **argv;
+    int time, mem, prior, i, argc = 0;
+    char cmdline[MAXLINE];
+
+    // clear buffer
+    memset(buffer, 0 , BUFFER_SIZE);
+
+    // get the command from the user input
+    printf("[submit] >> ");
+    if (!fgets(cmdline, sizeof(cmdline), stdin))
+        printf("Failed to fgets.\n");
+    else {
+        if (feof(stdin)) {
+            exit(0);
+        }
+
+        // prepare argv: tokenize user input, attach null char after each command
+        argv = (char **) malloc((strlen(cmdline) + 1) * sizeof(char *));
+
+        char *token = strtok(cmdline, DELIM);
+        while (token) {
+            size_t length = strlen(token) + 1;
+            argv[argc] = malloc(length);
+            memcpy(argv[argc], token, length);
+
+            argc++;
+
+            argvlen += length;
+            token = strtok(NULL, DELIM);
+        }
+        argv[argc] = NULL;
+
+        // prompt the user to enter and get maxmem, maxtime, priority
+        printf("(byte) max_mem_to_consume=");
+        scanf("%d", &mem);
+
+        printf("(sec) max_time_to_run=");
+        scanf("%d", &time);
+
+        printf("priority=");
+        scanf("%d", &prior);
+
+        // copy mem, time, priority, envplen, argvlen, argc, envp, argc in order
+        memcpy(buffer, &mem, sizeof(int));
+        joblen = sizeof(int);
+
+        memcpy(buffer+joblen, &time, sizeof(int));
+        joblen += sizeof(int);
+
+        memcpy(buffer+joblen, &prior, sizeof(int));
+        joblen += sizeof(int);
+
+        memcpy(buffer+joblen, &envplen, sizeof(int));
+        joblen += sizeof(int);
+
+        memcpy(buffer+joblen, &argvlen, sizeof(int));
+        joblen += sizeof(int);
+
+        memcpy(buffer+joblen, &argc, sizeof(int));
+        joblen += sizeof(int);
+
+        memcpy(buffer+joblen, *envp, strlen(*envp) + 1);
+        joblen += strlen(*envp) + 1;
+
+        for (i = 0; i < argc; i++) {
+            memcpy(buffer+joblen, argv[i], strlen(argv[i]) + 1);
+            joblen += strlen(argv[i]) + 1;
+        }
+    }
+
+    // free each argv[i]
+    for(i = 0; i < argc; i++)
+        free(argv[i]);
+    // free argv
+    free(argv);
+
+    return joblen;
+}
+
+void print_buf(unsigned char *buf, int len) {
+    int i;
+    unsigned char c;
+    for(i = 0; i < len; i ++) {
+        c = buf[i];
+        if(c == '\0')
+            printf("\\0");
+        else
+            printf("%x", buf[i]);
+    }
+    printf("\n");
+}
+
+// check user's cmd (submit, list, kill, exit),
+// parse cmd_type to 1, 2, 3 respectively
+int get_cmd_type(char *cmd, unsigned char packet[BUFFER_SIZE], char **envp) {
+    int cmd_type = 0;
+    int msglen = 0, joblen = 0;
+    unsigned char job[BUFFER_SIZE];
+
+    // cut the trailing line
+    cmd[strlen(cmd)-1] = '\0';
+
+    // clear the packet
+    memset(packet, 0, BUFFER_SIZE);
+
+    if (strcmp(cmd, "submit") == 0) {
+        cmd_type = 1;
+
+        joblen = submit(&job[0], envp);
+
+        msglen += sizeof(int) * 2;  // cmd_type + msglen
+        msglen += joblen;   // cmd_type + msglen + job
+
+        // set the 1st byte of the packet to cmd_type, 1
+        memcpy(packet, &cmd_type, sizeof(int));
+        // set the 2nd byte to the msglen
+        memcpy(packet+sizeof(int), &msglen, sizeof(int));
+        // set next bytes to the job
+        memcpy(packet + 2*sizeof(int), job, joblen);
+    }
+    else if (strcmp(cmd, "list") == 0) {
+        cmd_type = 2;
+        msglen = sizeof(int);
+
+        // set the 1st byte of the packet to cmd_type, 2
+        memcpy(packet, &cmd_type, sizeof(int));
+    }
+    else if (strcmp(cmd, "kill") == 0) {
+        cmd_type = 3;
+        int jobpid = 0;
+
+        msglen = 2 * sizeof(int); // one for cmd_type, another for pid
+
+        // prompt the user to enter jobpid to kill
+        printf("[kill] >> jobpid=");
+        scanf("%d", &jobpid);
+
+        // copy cmd_type and jobpid to packet
+        memcpy(packet, &cmd_type, sizeof(int));
+        memcpy(packet+sizeof(int), &jobpid, sizeof(int));
+    }
+    else if (strcmp(cmd, "exit") == 0) {
+        exit(0);
+    }
+
+    return msglen;
 }
 
 static void create_sock(){
@@ -35,6 +191,39 @@ static void create_sock(){
     char* file_path = strcat((char *)homedir, FILE_NAME);
     strcpy(server_addr.sun_path, file_path);
     connect(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+}
+
+int main(int argc, char** argv, char** envp) {
+    char cmdline[MAXLINE];
+    unsigned char packet[BUFFER_SIZE];
+    int msglen = 0, received_bytes = 0;
+    char recv_buffer[BUFFER_SIZE];
+
+    while(1) {
+        printf(">> ");
+        if (!fgets(cmdline, sizeof(cmdline), stdin))
+            printf("Failed to fgets.\n");
+        else {
+            if (feof(stdin)) {
+                exit(0);
+            }
+
+            // prepare the packet
+            msglen = get_cmd_type(cmdline, &packet[0], envp);
+            print_buf(&packet[0], msglen);
+
+            // set up the socket
+            create_sock();
+
+            int sent_bytes = send(sock_fd, packet, msglen, 0);
+        }
+
+//        if((received_bytes = recv(sock_fd, recv_buffer, RECV_BUFF_SIZE, 0)) < 0) {
+//            perror("receiving data");
+//            exit(1);
+//        }
+//        printf("%s\n", recv_buffer);
+    }
 }
 
 
